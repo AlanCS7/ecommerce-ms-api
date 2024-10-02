aff708f7a477fcbd48cf29d07dbdbf53437f4ca7
Alan Silva <alanclash2541@gmail.com>
2024-10-01 23:06:23 -0300

    feat: create and configure the discovery server

---
+++ discovery/src/test/java/dev/alancss/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alancss.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
