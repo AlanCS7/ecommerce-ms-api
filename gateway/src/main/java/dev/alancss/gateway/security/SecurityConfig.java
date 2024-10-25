35d68ae5d9ab47cc9f34f67e4a7300c05dffe2be
Alan Silva <alanclash2541@gmail.com>
2024-10-25 20:08:12 -0300

    feat: setup Keycloak for the security and security configuration for the API Gateway

---
+++ gateway/src/main/java/dev/alancss/gateway/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package dev.alancss.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("(/eureka/**)").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oaut2 -> oaut2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
